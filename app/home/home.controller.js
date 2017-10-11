ca1c9389c901ac7300a2289356b66c23f60856d5
Anand Dev Singh <ananddevsingh.aiet@gmail.com>
2017-10-12 00:44:58 +0530

    Stable code upto full rows

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,190 +1,198 @@
-(function () {
+(function (angular) {
     'use strict';
-
-    angular
-        .module('tambola')
-        .controller('HomeController', HomeController);
-
+    angular.module('tambola').controller('HomeController', HomeController);
     HomeController.$inject = ['$scope'];
 
-    function HomeController($scope) {
-
+    function HomeController() {
         var vm = this;
-        
         /************************* Seting up the initial state - START ************************/
-        
         vm.ticketList = [];
         vm.ticketModels = [];
-
         /*Fixed ticket*/
         var newTicket = new CreateTicket();
         vm.initTicketModels = newTicket.init();
-        
-        /*vm.initTicketModels = {
-            ticketNumber: 'A1001',
-            rows: [
+        vm.initTicketModels_test = {
+            ticketNumber: 'A1001'
+            , results: {
+                isFullHouse: false
+                , isCorner: false
+                , fullRows: []
+            }
+            , rows: [
                 {
                     'r1c1': {
-                        'value': '1',
-                        'isCrossed': true,
-                    },
-                    'r1c2': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r1c3': {
-                        'value': '26',
-                        'isCrossed': false,
-                    },
-                    'r1c4': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r1c5': {
-                        'value': '42',
-                        'isCrossed': false,
-                    },
-                    'r1c6': {
-                        'value': '51',
-                        'isCrossed': false,
-                    },
-                    'r1c7': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r1c8': {
-                        'value': '72',
-                        'isCrossed': false,
-                    },
-                    'r1c9': {
-                        'value': '',
-                        'isCrossed': false
-                    },
-            },
-                {
+                        'value': '1'
+                        , 'isCrossed': true
+                    , }
+                    , 'r1c2': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r1c3': {
+                        'value': '26'
+                        , 'isCrossed': true
+                    , }
+                    , 'r1c4': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r1c5': {
+                        'value': '42'
+                        , 'isCrossed': true
+                    , }
+                    , 'r1c6': {
+                        'value': '51'
+                        , 'isCrossed': true
+                    , }
+                    , 'r1c7': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r1c8': {
+                        'value': '72'
+                        , 'isCrossed': true
+                    , }
+                    , 'r1c9': {
+                        'value': ''
+                        , 'isCrossed': false
+                    }
+                , }
+                , {
                     'r2c1': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r2c2': {
-                        'value': '12',
-                        'isCrossed': false,
-                    },
-                    'r2c3': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r2c4': {
-                        'value': '39',
-                        'isCrossed': false,
-                    },
-                    'r2c5': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r2c6': {
-                        'value': '55',
-                        'isCrossed': false,
-                    },
-                    'r2c7': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r2c8': {
-                        'value': '76',
-                        'isCrossed': false,
-                    },
-                    'r2c9': {
-                        'value': '82',
-                        'isCrossed': false
-                    },
-            },
-                {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r2c2': {
+                        'value': '12'
+                        , 'isCrossed': true
+                    , }
+                    , 'r2c3': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r2c4': {
+                        'value': '39'
+                        , 'isCrossed': true
+                    , }
+                    , 'r2c5': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r2c6': {
+                        'value': '55'
+                        , 'isCrossed': false
+                    , }
+                    , 'r2c7': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r2c8': {
+                        'value': '76'
+                        , 'isCrossed': true
+                    , }
+                    , 'r2c9': {
+                        'value': '82'
+                        , 'isCrossed': false
+                    }
+                , }
+                , {
                     'r3c1': {
-                        'value': '8',
-                        'isCrossed': false,
-                    },
-                    'r3c2': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r3c3': {
-                        'value': '28',
-                        'isCrossed': false,
-                    },
-                    'r3c4': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r3c5': {
-                        'value': '45',
-                        'isCrossed': false,
-                    },
-                    'r3c6': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r3c7': {
-                        'value': '69',
-                        'isCrossed': false,
-                    },
-                    'r3c8': {
-                        'value': '',
-                        'isCrossed': false,
-                    },
-                    'r3c9': {
-                        'value': '87',
-                        'isCrossed': false
-                    },
-            }
+                        'value': '8'
+                        , 'isCrossed': true
+                    , }
+                    , 'r3c2': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r3c3': {
+                        'value': '28'
+                        , 'isCrossed': true
+                    , }
+                    , 'r3c4': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r3c5': {
+                        'value': '45'
+                        , 'isCrossed': true
+                    , }
+                    , 'r3c6': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r3c7': {
+                        'value': '69'
+                        , 'isCrossed': true
+                    , }
+                    , 'r3c8': {
+                        'value': ''
+                        , 'isCrossed': false
+                    , }
+                    , 'r3c9': {
+                        'value': '87'
+                        , 'isCrossed': false
+                    }
+                , }
             ]
-        };*/
-
+        };
         /************************* Seting up the initial state - END ************************/
-
         /************************* Adding behavior - START ************************************/
-
         vm.saveTicket = saveTicket;
         vm.clearTicket = clearTicket;
         vm.watchTickets = watchTickets;
-
         /************************* Adding behavior - END ************************************/
-
-
         /************************* Function definations - START ************************************/
-
         /* $scope.$watch('home.announcedNumber', function (newVal, oldVal) {
              console.log(newVal, '-', oldVal);
          })*/
-
         function clearTicket() {
             resetTicketValues(vm.initTicketModels);
         }
 
-        function watchTickets(action) {            
+        function watchTickets(action) {
             switch (action) {
-                case 'cross':
-                    scanTickets(vm.crossNumber, true);
-                    vm.crossNumber = '';
-                    break;
-                case 'clear':
-                    scanTickets(vm.clearNumber, false);
-                    vm.clearNumber = '';
-                    break;
+            case 'cross':
+                scanTickets(vm.crossNumber, true);
+                vm.crossNumber = '';
+                break;
+            case 'clear':
+                scanTickets(vm.clearNumber, false);
+                vm.clearNumber = '';
+                break;
             }
-
         }
 
         function scanTickets(number, isCrossed) {
             if (number) {
+                var fullRowsIndex;
                 angular.forEach(vm.ticketList, function (listItem, listIndex) {
                     angular.forEach(listItem.rows, function (rowItem, rowIndex) {
-                        angular.forEach(rowItem, function (objValue, objKey) {
-                            if (objValue['value'] == number) {
-                                objValue['isCrossed'] = isCrossed;
+                        // Set the announced value.
+                        angular.forEach(rowItem, function (item, index) {
+                            //1: Cross the selected number.
+                            if (item['value'] == number) {
+                                item['isCrossed'] = isCrossed;
                             }
-                        })
-                    })
+                        });
+                        // Look for full row.
+                        var isOpenValue = false;
+                        for (var itemKey in rowItem) {
+                            fullRowsIndex = rowIndex;
+                            if (rowItem[itemKey]['value'] && !rowItem[itemKey]['isCrossed']) {
+                                fullRowsIndex = undefined;
+                                break;
+                            }
+                        };                        
+                        if (typeof fullRowsIndex !== 'undefined') {
+                            var rowsArray = vm.ticketList[listIndex]['results']['fullRows'];
+                            rowsArray.push(fullRowsIndex);
+                            var uniqueArray = rowsArray.filter(function (item, pos) {
+                                return rowsArray.indexOf(item) == pos;
+                            });
+                            vm.ticketList[listIndex]['results']['fullRows'] = uniqueArray.sort();
+                            //console.log(vm.ticketList[listIndex]['results']);
+                        }
+                    });
                 });
             }
         }
@@ -195,7 +203,6 @@
             //angular.element('[data-dismiss="modal"]').trigger('click');                      
         }
 
-
         function resetTicketValues(values) {
             values = values || vm.initTicketModels;
             for (var keys in values) {
@@ -205,9 +212,7 @@
                 }
             }
         }
-
         /************************* Function definations - END ************************************/
-
         vm.getTemplate = function (templateName) {
             return ''.concat('./app/home/templates/', templateName, '.html');
         }
@@ -217,8 +222,13 @@
             this.rowsCount = rowsCount || 3;
             this.columnsCount = columnsCount || 9;
             this.baseModel = {
-                ticketNumber: '',
-                rows: []
+                ticketNumber: ''
+                , results: {
+                    isFullHouse: false
+                    , isCorner: false
+                    , fullRows: []
+                }
+                , rows: []
             };
             this.createMatrix = function () {
                 this.baseModel.ticketNumber = this.ticketNumber;
@@ -226,8 +236,8 @@
                     var row = {}
                     for (var j = 1; j <= this.columnsCount; j++) {
                         row[''.concat('r', i, 'c', j)] = {
-                            value:  '',
-                            isClosed: false
+                            value: ''
+                            , isClosed: false
                         };
                     }
                     this.baseModel.rows.push(row);
@@ -239,5 +249,4 @@
             }
         }
     };
-
-})();
+})(angular);
